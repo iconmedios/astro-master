dbe7d7e138e619fb613ca032237cafcaed00e43d
iconmedios <jean@iconmedios.com>
2023-12-19 21:53:49 -0600

    Scrool navigation creada con enlace activo

--- src/app.js
+++ src/app.js
@@ -1,107 +1,133 @@
 console.log('hola');
 // import checkScreen from './js/mediamatch'
 
-( (d,w,c)=>{
+((d, w) => {
+  selectorThemes();
 
-   
-// NAVBAR FIXED
-const navBar = document.querySelector("#navbar")
-window.addEventListener("scroll",  ()=> {
-    let posScroll = window.scrollY 
+
+
+  // NAVBAR FIXED
+  const navBar = d.querySelector("#navbar")
+  window.addEventListener("scroll", () => {
+    let posScroll = w.scrollY
     navBar.classList.toggle("sticky", posScroll > 0)
-})
-// NAVBAR FIXED
+  })
+  // NAVBAR FIXED
 
+  // MENU MOBILE
+  // const btnOpen = d.querySelector("#button-open") 
+  // const navMobile = d.querySelector("#nav-mobile") 
+
+  // function openMenuMob () { navMobile.classList.toggle("visible");}
+  // function closeMenuMob () { navMobile.classList.remove("visible");}
+  // btnOpen.addEventListener("click", openMenuMob);
+  // navMobile.addEventListener("click", ()=>{ closeMenuMob() });
+  // MENU MOBILE
+
+  // BUTTON GO TOP
+
+  // const btnTop = d.querySelector("#button-go-top")
+  // w.addEventListener("scroll", () => {
+  //   btnTop.classList.toggle("visible", (w.scrollY > 400))
+
+  // })
+
+ // MENU MOBILE
+    const btnOpen = document.querySelector("#button-open") 
+    const navMobile = document.querySelector("#nav-mobile") 
+    
+    function openMenuMob () { navMobile.classList.toggle("visible");}
+    function closeMenuMob () { navMobile.classList.remove("visible");}
+    btnOpen.addEventListener("click", openMenuMob);
+    navMobile.addEventListener("click", ()=>{ closeMenuMob() });
 // MENU MOBILE
-const btnOpen = document.querySelector("#button-open") 
-const navMobile = document.querySelector("#nav-mobile") 
 
-function openMenuMob () { navMobile.classList.toggle("visible");}
-function closeMenuMob () { navMobile.classList.remove("visible");}
-btnOpen.addEventListener("click", openMenuMob);
-navMobile.addEventListener("click", ()=>{ closeMenuMob() });
-// MENU MOBILE
+  // MEDIA MACTCH  ///////////////////
 
-// BUTTON GO TOP
-const btnTop = document.querySelector("#button-go-top")
-window.addEventListener("scroll",  ()=> {
-btnTop.classList.toggle("visible", (window.scrollY > 400 ) )
-    })
-// BUTTON GO TOP    
-	
-// AL REDIMENSIONAR VENTANA
-const onWindowSize = ()=> {
-//console.log( "W:", window.innerWidth, "H",  window.innerHeight);
-applyMatch(mql)
-}
-w.onresize = onWindowSize;
-// AL REDIMENSIONAR VENTANA
-// MEDIA MACTCH 
+  // FIN MEDIA MACTCH  ///////////////
 
-const mql = window.matchMedia('(max-width:900px)');
-const applyMatch = (mql ) => {
- 
-mql.matches ? console.log('mobile') : closeMenuMob();
+  // SCROLL NAVIGATION ///////////////////////////////////
+  d.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
-};
-applyMatch(mql)
-// MEDIA MACTCH 
+      const targetElement = document.querySelector(this.getAttribute('href'));
 
-// SCROLL NAVIGATION
-function scrollTo() {
-    const links = document.querySelectorAll(".scroll");
-    links.forEach((each) => (each.onclick = scrollAnchors));
-  }
-  
-  function scrollAnchors(e, respond = null) {
-    const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
-    e.preventDefault();
-    var targetID = respond
-      ? respond.getAttribute("href")
-      : this.getAttribute("href");
-    const targetAnchor = document.querySelector(targetID);
-    if (!targetAnchor) return;
-    const originalTop = distanceToTop(targetAnchor);
-    window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
-    const checkIfDone = setInterval(function () {
-      const atBottom =
-        window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-      if (distanceToTop(targetAnchor) === 0 || atBottom) {
-        targetAnchor.tabIndex = "-1";
-        targetAnchor.focus();
-        window.history.pushState("", "", targetID);
-        clearInterval(checkIfDone);
-      }
-    }, 100);
-  }
-  scrollTo();
-// SCROLL NAVIGATION
+      // Ajusta estos valores según tus preferencias
+      const scrollOptions = {
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+        speed: 500, // milisegundos
+        easing: 'easeOutElastic'
+      };
 
-      let videoStand = document.querySelectorAll("video.fslightbox-source");
-      
+      targetElement.scrollIntoView(scrollOptions);
+    });
+  })
+  // CON GSAP >>>>>>><
 
-			fsLightboxInstances["videostand"].props.onOpen = function () {
-				//window.scrollTo(0, 500);
-        console.log(videoStand);
-        async function playVideo() {
-          try {
-            await videoStand.play();
-            playButton.className = "playing";
-          } catch (err) {
-            playButton.className = "";
-          }
+
+
+  // SCROLL NAVIGATION ///////////////////////////////////
+
+  // AL CERRAR VENTANA POPUP EJECUTAR ACCION  ///////////////////////////
+
+  // let videoStand = document.querySelectorAll("video.fslightbox-source");
+
+
+  // fsLightboxInstances["videostand"].props.onOpen = function () {
+  // 	//window.scrollTo(0, 500);
+  //   console.log(videoStand);
+  //   async function playVideo() {
+  //     try {
+  //       await videoStand.play();
+  //       playButton.className = "playing";
+  //     } catch (err) {
+  //       playButton.className = "";
+  //     }
+  //   }
+  // }
+
+  // fsLightboxInstances["videostand"].props.onClose = function () {
+  // 	//console.log("Se cerro");
+
+  //   location.href = "#footer";
+
+  // }
+  // FIN AL CERRAR VENTANA POPUP EJECUTAR ACCION ///////////////////////////    
+
+  // SELECTOR DE TEMAS /////////////////////////////////////////
+
+  function selectorThemes() {
+    const mainTheme = document.querySelector('[data-theme]')
+    const themeBtns = document.querySelectorAll('.theme-btn');
+
+    // Obtener el tema almacenado en localStorage al cargar la página
+    const storedTheme = localStorage.getItem('selectedTheme');
+    if (storedTheme) {
+      mainTheme.setAttribute('data-theme', storedTheme);
+    }
+
+    themeBtns.forEach((themeBtn) => {
+      themeBtn.addEventListener('click', () => {
+        const nameTheme = themeBtn.getAttribute('name-theme');
+        if (nameTheme) {
+          // Establecer el tema en el atributo 'data-theme'
+          mainTheme.setAttribute('data-theme', nameTheme);
+
+          // Almacenar el tema seleccionado en localStorage
+          localStorage.setItem('selectedTheme', nameTheme);
         }
-			}
-		
-			fsLightboxInstances["videostand"].props.onClose = function () {
-				//console.log("Se cerro");
-       
-        location.href = "#footer";
-        
-			}
+      });
+    });
 
+  }
 
+  // FIN SELECTOR DE TEMAS /////////////////////////////////////////
 
-} )(document, window, console)
+// Get all sections that have an ID defined
+
+})(document, window)
 
 
